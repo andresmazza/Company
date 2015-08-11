fe7bb7c890cdb1a3138e4c7b674c8ccf3181f3ba
andresmazza <andres.mazza@gmail.com>
2015-08-10 23:11:15 -0300

    EmployeeFactory

--- index.php
+++ index.php
@@ -10,28 +10,20 @@ $employee = EmployeeFactory::build('Developer');
 $employee->setId(1);
 $employee->setName('ANDRES');
 $employee->setLastname('MAZZA');
-$employee->setAge(3);
+$employee->setAge(20);
 $employee->setType(DeveloperType::skill('PHP'));
 $company->addEmployee($employee);
 
 $employee = EmployeeFactory::build('Developer');
 $employee->setId(2);
-$employee->setName('JUAN');
-$employee->setLastname('LALA');
-$employee->setAge(3);
-$employee->setType(DeveloperType::skill('PHP'));
-$company->addEmployee($employee);
-
-$employee = EmployeeFactory::build('Developer');
-$employee->setId(3);
 $employee->setName('PEDRO');
 $employee->setLastname('PICAPIEDRA');
-$employee->setAge(7);
+$employee->setAge(22);
 $employee->setType(DeveloperType::skill('NET'));
 $company->addEmployee($employee);
 
 $employee = EmployeeFactory::build('Designer');
-$employee->setId(4);
+$employee->setId(3);
 $employee->setName('NAME');
 $employee->setLastname('LASTNAME');
 $employee->setAge(34);
@@ -39,23 +31,14 @@ $employee->setType(Types\DesignerType::skill('Web'));
 $company->addEmployee($employee);
 
 $employee = EmployeeFactory::build('Designer');
-$employee->setId(5);
+$employee->setId(4);
 $employee->setName('NAME');
 $employee->setLastname('LASTNAME');
 $employee->setAge(33);
 $employee->setType(Types\DesignerType::skill('Graphics'));
 $company->addEmployee($employee);
 
-$employee = EmployeeFactory::build('Designer');
-$employee->setId(6);
-$employee->setName('NAME');
-$employee->setLastname('LASTNAME');
-$employee->setAge(33);
-$employee->setType(Types\DesignerType::skill('Web'));
-$company->addEmployee($employee);
-
-
-$current = $company->findById(6);
+$current = $company->findById(1);
 echo $current->getType()->skill();
 echo "\n";
 echo $company->getEmployeesAvg();
